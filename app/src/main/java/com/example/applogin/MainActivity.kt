480f86745ee24cb071b1a06447c98f8f8ebc6eda
Iván Estépar <ivanestepar999@gmail.com>
2024-10-13 14:18:06 +0100

    Modificaciones minimas

--- app/src/main/java/com/example/applogin/MainActivity.kt
+++ app/src/main/java/com/example/applogin/MainActivity.kt
@@ -29,18 +29,26 @@ class MainActivity : AppCompatActivity() {
             val password = editPassword.text.toString()
 
             if (username.isNotEmpty() && password.isNotEmpty()) {
+                // Validar que la contraseña tenga más de 8 caracteres
+                if (password.length <= 8) {
+                    Toast.makeText(this, "La contraseña debe tener más de 8 caracteres", Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener // Salir del listener si la contraseña no es válida
+                }
+
                 lifecycleScope.launch {
                     val user = app.room.userDao().getUser(username, password)
 
                     if (user == null) {
+                        // El usuario no existe, se puede registrar
                         val newUser = User(id = 0, name = username, password = password)
                         app.room.userDao().insert(newUser)
                         withContext(Dispatchers.Main) {
                             Toast.makeText(this@MainActivity, "Usuario registrado", Toast.LENGTH_SHORT).show()
                         }
                     } else {
+                        // El usuario ya existe
                         withContext(Dispatchers.Main) {
-                            Toast.makeText(this@MainActivity, "Bienvenido ${user.name}", Toast.LENGTH_SHORT).show()
+                            Toast.makeText(this@MainActivity, "El usuario ya existe", Toast.LENGTH_SHORT).show()
                         }
                     }
                 }
@@ -48,5 +56,6 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Por favor, completa los campos", Toast.LENGTH_SHORT).show()
             }
         }
+
     }
 }
